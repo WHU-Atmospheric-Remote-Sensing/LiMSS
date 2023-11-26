4ba7b4e9855f27290f609d6fd902eff95d7abb57
Zhenping Yin <zp.yin@whu.edu.cn>
2023-11-26 15:06:27 +0800

    backup

---
+++ lib/rayleigh_brillouin/calcCollisionFreq.m
@@ -0,0 +1,19 @@
+function [y] = calcCollisionFreq(params)
+% CALCCOLLISIONFREQ description
+%
+% USAGE:
+%    [y] = calcCollisionFreq(params)
+%
+% INPUTS:
+%    params
+%
+% OUTPUTS:
+%    y
+%
+% EXAMPLE:
+%
+% HISTORY:
+%    2023-08-29: first edition by Zhenping
+% .. Authors: - zp.yin@whu.edu.cn
+
+end
